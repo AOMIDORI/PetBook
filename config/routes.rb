b09ef1c207b3d882565d9e803ecf58ae08a7dbb8
AOMIDORI <alexalexchou@outlook.com>
2016-11-06 17:04:44 +0200

    log-in

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  #get 'sessions/create'
   get 'users/new'
 
-  resources :users
   get 'welcome/index'
 
   root 'welcome#index'
 
   get '/signup', to:'users#new'
+  get '/login', to:'sessions#new'
+  post '/login', to:'sessions#create'
+  delete '/logout', to:'sessions#destroy'
+
   resources :users
+  resources :sessions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
