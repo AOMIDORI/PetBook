550225eebd4b005d7b0cf274b5e7958104b5ee2e
AOMIDORI <alexalexchou@outlook.com>
2016-11-07 10:27:05 +0200

    route fixing

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,19 @@
 Rails.application.routes.draw do
-  get 'static_pages/about'
 
-  get 'sessions/new'
   #get 'sessions/create'
-  get 'users/new'
-
   get 'welcome/index'
 
   root 'welcome#index'
 
+  get '/about', to: 'static_pages#about'
   get '/signup', to:'users#new'
   get '/login', to:'sessions#new'
   post '/login', to:'sessions#create'
   delete '/logout', to:'sessions#destroy'
 
+
   resources :users
-  resources :sessions
+  resources :account_activations, only: [:edit]
+  #resources :sessions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161106151944) do
+ActiveRecord::Schema.define(version: 20161106201518) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,10 +18,14 @@ ActiveRecord::Schema.define(version: 20161106151944) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
     t.string   "password_digest"
     t.string   "remember_digest"
+    t.boolean  "admin",             default: false
+    t.string   "activation_digest"
+    t.boolean  "activated",         default: false
+    t.datetime "activated_at"
   end
 
 end
