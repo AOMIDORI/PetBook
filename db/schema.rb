d47fbe9ab75fad818d28942db8388bdc72527bef
Susanna Nevalainen <susanna.nevalainen@aalto@fi>
2016-11-15 00:32:04 +0200

    button color changed, small changes to style

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161108195349) do
+ActiveRecord::Schema.define(version: 20161109162518) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pets", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_pets_on_user_id", using: :btree
+  end
 
   create_table "posts", force: :cascade do |t|
     t.text     "content"
@@ -48,5 +56,6 @@ ActiveRecord::Schema.define(version: 20161108195349) do
     t.datetime "activated_at"
   end
 
+  add_foreign_key "pets", "users"
   add_foreign_key "posts", "users"
 end
