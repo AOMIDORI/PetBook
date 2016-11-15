b046846d2cf209bd06733f84ec51f0ae5d1353ea
Susanna Nevalainen <susanna.nevalainen@aalto@fi>
2016-11-15 02:05:50 +0200

    pet profile draft added

---
+++ app/controllers/pets_controller.rb
@@ -0,0 +1,9 @@
+class PetsController < ApplicationController
+  protect_from_forgery with: :exception
+  def petprofile
+    #if logged_in?
+    #  redirect_to home_url
+    #end
+  end
+  #include SessionsHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,13 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
+#Define which html.erb file the address ending links to
   get '/home', to: 'static_pages#home'
   get '/about', to: 'static_pages#about'
   get '/contact', to:'static_pages#contact'
   get '/signup', to:'users#new'
   get '/login', to:'sessions#new'
+  get '/petprofile', to:'pets#petprofile'
   post '/login', to:'sessions#create'
   delete '/logout', to:'sessions#destroy'
 
