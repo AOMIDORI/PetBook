7809ee76480389bc893baf65095641fac4574e75
AOMIDORI <alexalexchou@outlook.com>
2016-12-30 01:27:42 +0100

    c

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy, :like]
-  before_action :correct_user, only: :destroy
+  before_action :correct_user, only: [:create, :destroy, :like]
 
   def index
     @posts = Post.all
